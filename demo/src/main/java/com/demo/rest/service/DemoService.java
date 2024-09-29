9acccaa830a5b9e2b807d059d7400ea30fcb12c2
yadavprerana455 <yadavprerana455@gmail.com>
2024-09-29 15:06:59 +0530

    method added in service class

--- demo/src/main/java/com/demo/rest/service/DemoService.java
+++ demo/src/main/java/com/demo/rest/service/DemoService.java
@@ -1,8 +1,12 @@
 package com.demo.rest.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Service
 public class DemoService {
-
+	@RequestMapping("/")
+	public void hello() {
+		
+	}
 }
